ca60324a59b9fa9ff0455cdd18e9bf126e855fe4
Brett DeWoody <brett@firstbasehq.com>
2022-05-06 20:12:12 +0100

    Fix build and other errors.

--- .eleventy.js
+++ .eleventy.js
@@ -6,7 +6,7 @@ module.exports = function (eleventyConfig) {
       includes: "/_11ty/_includes/",
       layouts: "/_11ty/_layouts/",
       data: "/_11ty/_data/",
-      output: "./public/",
+      output: "./_public/",
     },
   };
 };
